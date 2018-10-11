4b44bfd580c054a631bc90f6f1e883dca6792598
Rivka Farizi <rivkafarizzi@gmail.com>
2018-10-11 15:32:07 +0700

    migration

--- app/Models/Assign_location.php
+++ app/Models/Assign_location.php
@@ -18,3 +18,4 @@ class Assign_location extends Model
     }
     
 }
+ 
--- resources/views/vendor/backpack/base/inc/sidebar.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar.blade.php
@@ -12,25 +12,33 @@
           <!-- ================================================ -->
           <!-- ==== Recommended place for admin menu items ==== -->
           <!-- ================================================ -->
-          <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
-          <li><a href="{{ backpack_url ('user') }}"><i class="fa fa-users"></i> <span>{{ trans('Manage Account') }}</span></a></li>
+          @if (Auth::user()->hasRole('admin'))
+            <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
+            <li><a href="{{ backpack_url ('user') }}"><i class="fa fa-users"></i> <span>{{ trans('Manage Account') }}</span></a></li>
 
-          <li class="treeview">
-          <a href="#"><i class="fa-fa-archieve"></i>Master Data<i class="fa fa-angle-left pull-right"></i></a>
-          <ul class="treeview-menu">
-            <li><a href="{{ backpack_url('employee') }}"><i class="fa fa-address-book"></i> <span>{{ trans('Employee') }}</span></a></li>
-            <li><a href="{{ backpack_url('departure_time') }}"><i class="fa fa-bus"></i> <span>{{ trans('Departure Time') }}</span></a></li>
-            <li><a href="{{ backpack_url('destination') }}"><i class="fa fa-clipboard"></i> <span>{{ trans('Destination') }}</span></a></li>
-            <li><a href="{{ backpack_url('work_time') }}"><i class="fa fa-clock-o"></i> <span>{{ trans('Work Time') }}</span></a></li>
-            <li><a href="{{ backpack_url('assign_location') }}"><i class="fa fa-location-arrow"></i> <span>{{ trans('Assign Location') }}</span></a></li>
-          </ul>
-          </li>
-             <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-clipboard"></i> <span>{{ trans('Order') }}</span></a></li>
-             <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-archive"></i> <span>{{ trans('EOD') }}</span></a></li>
-             <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-book"></i> <span>{{ trans('Manifest') }}</span></a></li>
+            <li class="treeview">
+            <a href="#"><i class="fa-fa-archieve"></i>Master Data<i class="fa fa-angle-left pull-right"></i></a>
+              <ul class="treeview-menu">
+                <li><a href="{{ backpack_url('employee') }}"><i class="fa fa-address-book"></i> <span>{{ trans('Employee') }}</span></a></li>
+                <li><a href="{{ backpack_url('departure_time') }}"><i class="fa fa-bus"></i> <span>{{ trans('Departure Time') }}</span></a></li>
+                <li><a href="{{ backpack_url('destination') }}"><i class="fa fa-clipboard"></i> <span>{{ trans('Destination') }}</span></a></li>
+                <li><a href="{{ backpack_url('work_time') }}"><i class="fa fa-clock-o"></i> <span>{{ trans('Work Time') }}</span></a></li>
+                <li><a href="{{ backpack_url('assign_location') }}"><i class="fa fa-location-arrow"></i> <span>{{ trans('Assign Location') }}</span></a></li>
+              </ul>
+            </li>
+              <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-clipboard"></i> <span>{{ trans('Order') }}</span></a></li>
+              <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-archive"></i> <span>{{ trans('EOD') }}</span></a></li>
+          @endif
+
+          @if (Auth::user()->hasRole('leader'))
+            <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-book"></i> <span>{{ trans('Manifest') }}</span></a></li>
+          @endif
+          
+          @if (Auth::user()->hasRole('admin'))
             <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-bell"></i> <span>{{ trans('Approve EOD') }}</span></a></li>
-             <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-clipboard"></i> <span>{{ trans('Report ') }}</span></a></li>
-             <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-bar-chart"></i> <span>{{ trans('View Board ') }}</span></a></li>
+            <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-clipboard"></i> <span>{{ trans('Report ') }}</span></a></li>
+            <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-bar-chart"></i> <span>{{ trans('View Board ') }}</span></a></li>
+          @endif  
 
           <!-- ======================================= -->
           {{-- <li class="header">Other menus</li> --}}
