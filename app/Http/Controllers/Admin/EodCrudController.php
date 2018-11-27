8a279eb1f7657c675ea9690945dd897a95b016ed
Agung Gunawan <lembagax@gmail.com>
2018-11-26 23:09:46 -0800

    Hapus filter AssignLocatio, WorkTime. Dan add column AssignLocation, WorkTime

--- app/Http/Controllers/Admin/EodCrudController.php
+++ app/Http/Controllers/Admin/EodCrudController.php
@@ -25,6 +25,22 @@ class EodCrudController extends CrudController
         }
         $this->crud->denyAccess(['create', 'update', 'delete']);
         $this->crud->addColumn([
+            'label' => "Assign Location",
+            'name' => 'assign_location_id',
+            'type' => 'select',
+            'entity' => 'assignLocation',
+            'attribute' => 'assign_location',
+            'model' => 'App\AssignLocation'
+        ]);
+        $this->crud->addColumn([
+            'label' => "Work Time",
+            'name' => 'work_time_id',
+            'type' => 'select',
+            'entity' => 'workTime',
+            'attribute' => 'work_time',
+            'model' => 'App\WorkTime'
+        ]);
+        $this->crud->addColumn([
             'label' => "Ticketing",
             'type' => "employee",
             'name' => 'user_id'
@@ -48,15 +64,6 @@ class EodCrudController extends CrudController
             'name' => 'pdf',
             'type' => 'eod_pdf'
         ]);
-        $this->crud->addColumn([
-            'label' => "Assign Location",
-            'name' => 'assign_location_id',
-            'type' => 'select',
-            'entity' => 'assignLocation',
-            'attribute' => 'assign_location',
-            'model' => 'App\AssignLocation'
-        ]);
-
         $this->crud->addFilter([
             'type' => 'date',
             'name' => 'created_at',
@@ -66,38 +73,38 @@ class EodCrudController extends CrudController
         function($value) {
             $this->crud->addClause('where', 'created_at', 'like', $value .'%');
         });
-        $assign_locations = [];
-        foreach (AssignLocation::all() as $assign_location) {
-            $assign_locations = array_merge($assign_locations, [$assign_location->code_location => $assign_location->assign_location]);
-        }
-        $this->crud->addFilter([
-            'type' => 'dropdown',
-            'name' => 'assign_location',
-            'label'=> 'Assign Location'
-        ],
-        $assign_locations,
-        function($value) {
-            $value = AssignLocation::where('code_location', $value)->first();
-            $this->crud->addClause('where', 'assign_location_id', $value->id);
-        });
 
-        $work_times = [];
-        foreach (WorkTime::all() as $work_time) {
-            $work_times = array_merge($work_times, ["'" . $work_time->id . "'" => $work_time->work_time . " - " . $work_time->assignLocation->assign_location]);
-        }
-        $this->crud->addFilter([
-            'type' => 'dropdown',
-            'name' => 'work_time',
-            'label'=> 'Work Time'
-        ],
-        $work_times,
-        function($value) {
-            $value = WorkTime::find(str_replace("'", "", $value));
-            $this->crud->addClause('where', 'work_time_id', $value->id);
-        });
-        
         if (Auth::user()->hasRole('admin')) {
             $this->crud->addButtonFromView('line', 'approve', 'eod_approve', 'end');
+            $assign_locations = [];
+            foreach (AssignLocation::all() as $assign_location) {
+                $assign_locations = array_merge($assign_locations, [$assign_location->code_location => $assign_location->assign_location]);
+            }
+            $this->crud->addFilter([
+                'type' => 'dropdown',
+                'name' => 'assign_location',
+                'label'=> 'Assign Location'
+            ],
+            $assign_locations,
+            function($value) {
+                $value = AssignLocation::where('code_location', $value)->first();
+                $this->crud->addClause('where', 'assign_location_id', $value->id);
+            });
+
+            $work_times = [];
+            foreach (WorkTime::all() as $work_time) {
+                $work_times = array_merge($work_times, ["'" . $work_time->id . "'" => $work_time->work_time . " - " . $work_time->assignLocation->assign_location]);
+            }
+            $this->crud->addFilter([
+                'type' => 'dropdown',
+                'name' => 'work_time',
+                'label'=> 'Work Time'
+            ],
+            $work_times,
+            function($value) {
+                $value = WorkTime::find(str_replace("'", "", $value));
+                $this->crud->addClause('where', 'work_time_id', $value->id);
+            });
         } else {
             $this->crud->addClause('where', 'user_id', Auth::user()->id);
             $this->crud->removeColumns(['user_id']);
